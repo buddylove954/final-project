5a0d41d676b83d83adf9f46ca7200a640fa4731e
will love <wlove29@gmail.com>
2015-04-27 12:33:53 -0400

    twitch api is working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,9 @@ class UsersController < ApplicationController
 	before_action :authenticate_user!
 
 	def home
+		@twitch = Twitch.new
+	    @result = @twitch.getStream("itshafu")
+	    @response = @result[:body]['_links']['channel']
 		render(:home)
 	end
 end
