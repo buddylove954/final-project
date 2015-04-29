86917b8864a1a4647cb85b574b2f8e5e7fbaad2b
will love <wlove29@gmail.com>
2015-04-29 13:36:21 -0400

    finshed profile pic

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -7,6 +7,6 @@ class RegistrationsController < Devise::RegistrationsController
   end
 
   def account_update_params
-    params.require(:user).permit(:first_name, :topgame, :email, :profile_pic :password, :password_confirmation, :current_password)
+    params.require(:user).permit(:first_name, :topgame, :email, :avatar, :password, :password_confirmation, :current_password)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150423201949) do
+ActiveRecord::Schema.define(version: 20150429160503) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20150423201949) do
     t.string   "topgame"
     t.string   "profile_pic"
     t.string   "name"
+    t.string   "avatar"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
