df032531e7c550b514623e34dc8a5a7c7fedd8bc
will love <wlove29@gmail.com>
2015-05-01 12:38:49 -0400

    finshed games icons

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   $GAME_DROPDOWN = [["League of Legends"], ["Hearthstone: Heroes of Warcraft"], ["Dota 2"],
 ["Counter-Strike: Global Offensive"], ["Minecraft"], ["Grand Theft Auto V"], ["StarCraft II: Heart of the Swarm"],
 ["Garry's Mod"], ["Poker"], ["ArmA III"], ["Mortal Kombat X"], ["Diablo III: Reaper of Souls"],
-["Call of Duty: Advanced Warfare"], ["World of Warcraft: Warlords of Draenor"], ["FIFA 15 "], 
+["Call of Duty: Advanced Warfare"], ["World of Warcraft: Warlords of Draenor"], ["FIFA 15"], 
 ["RuneScape"], ["Destiny"], ["Bloodborne"], ["The Witcher 2: Assassins of Kings"], ["World of Tanks"], 
 ["Smite"], ["World of Warships"], ["DayZ"], ["Path of Exile"], ["Killing Floor 2"], ["Heroes of the Storm"],
 ["Pokémon Omega Ruby/Alpha Sapphire"], ["Borderlands: The Pre-Sequel!"], ["H1Z1"], ["Dragon Ball XenoVerse"],
