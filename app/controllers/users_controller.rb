7af1b90a22777d00c47fdfb3f8038277458ddb85
will love <wlove29@gmail.com>
2015-05-01 10:40:16 -0400

    changed margins in sidebar to % and added more fluff data

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,19 +3,23 @@ class UsersController < ApplicationController
 
 	def home
 		@twitch = Twitch.new
-		@streams = @twitch.getStreams[:body]["streams"]
+		@streams = @twitch.getStreams({"game"=>current_user.topgame})[:body]["streams"]
 		@arr = @streams.map do |stream|
-			if stream["game"] == "League of Legends" && (stream["_id"] != nil && stream["_links"]["self"] != nil)
+			if stream["game"] == current_user.topgame && (stream["_id"] != nil && stream["_links"]["self"] != nil)
 			{id: stream["_id"], game: stream["game"],
 			link: stream["_links"]["self"].split('/streams/')[1],
 			 viewers: stream["viewers"]}
 			end
+				
 		end
 		@arr = @arr.delete_if {|array| array == nil}
+		puts @arr
+		puts current_user.topgame
 		@game = @arr.sample
-		@logo = @twitch.searchGames({"q"=>"#{@game[:game]}",
-		 "type"=>"suggest"})[:body]["games"][0]["logo"]["medium"]
+		@logo = @twitch.searchGames({"q"=>"#{@game[:game]}","type"=>"suggest"})[:body]["games"][0]["logo"]["medium"] 
 		@users = User.all
 		render(:home)
+		# @game = @twitch.getStreams("game"=>current_user.topgame)[:body]
+		# render json: @game
 	end
 end
