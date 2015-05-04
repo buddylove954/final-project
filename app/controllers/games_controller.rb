64bca2c6293486ffbd853aa1f1b4aa220923b092
will love <wlove29@gmail.com>
2015-05-04 17:34:07 -0400

    game page has a twitch feed now

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -17,6 +17,19 @@ class GamesController < ApplicationController
 		@games = Game.all
 	end
 
+	def show
+		@twitch = Twitch.new
+		@game_idx = params[:id].to_i
+		@streams = @twitch.getStreams({"game"=>$GAME_DROPDOWN[@game_idx][0]})[:body]["streams"]
+		@arr = @streams.map do |stream|
+			{id: stream["_id"], game: stream["game"],
+			link: stream["_links"]["self"].split('/streams/')[1],
+			 viewers: stream["viewers"]}
+			end	
+			puts @arr[0][:game]		 
+		@logo = @twitch.searchGames({"q"=>"#{@arr[0][:game]}","type"=>"suggest"})[:body]["games"][0]["logo"]["medium"]
+	end
+
 	private
 
 	def games_params
