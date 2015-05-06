bccabad91ee4285ade877881c4059ce553854f0b
will love <wlove29@gmail.com>
2015-05-06 12:39:25 -0400

    finished css for player specific page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -31,6 +31,25 @@ class UsersController < ApplicationController
 	end
 
 	def show
-
+		@user = User.find(params[:id])
+		@twitch = Twitch.new
+		@streams = @twitch.getStreams({"game"=>@user.topgame})[:body]["streams"]
+		@arr = @streams.map do |stream|
+			if stream["game"] == @user.topgame && (stream["_id"] != nil && stream["_links"]["self"] != nil)
+			{id: stream["_id"], game: stream["game"],
+			link: stream["_links"]["self"].split('/streams/')[1],
+			 viewers: stream["viewers"]}
+			end
+				
+		end
+		@games = Game.where(user_id: @user.id)
+		@games = @games.sample(2)
+		@arr = @arr.delete_if {|array| array == nil}
+		puts @arr
+		puts @user.topgame
+		@game = @arr.sample
+		@logo = @twitch.searchGames({"q"=>"#{@game[:game]}","type"=>"suggest"})[:body]["games"][0]["logo"]["medium"] 
+		@users = User.all.sample(40)
+		render(:show)
 	end
 end
