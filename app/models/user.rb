2c674d303f44e39963dd76304d8a5a449ad8d431
will love <wlove29@gmail.com>
2015-06-03 17:13:48 -0400

    generated posts controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,7 +18,7 @@
   has_many :following, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
 
-  has_many :posts
+  has_many :posts, dependent: :destroy
 
     # Follows a user.
   def follow(other_user)
